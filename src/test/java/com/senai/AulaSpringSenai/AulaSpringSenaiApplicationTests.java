09929910301a5790424a5a2b4297e2f68f1cc5c7
Dev-VitorPereira <dev.vitorpereira@gmail.com>
2025-04-26 09:48:05 -0300

    feat criando projeto aula

---
+++ src/test/java/com/senai/AulaSpringSenai/AulaSpringSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.AulaSpringSenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
